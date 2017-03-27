e20af859903d13e22330983ce22e44a2bab111c7
YoanEnchev <joanbojchev@abv.bg>
2017-03-27 22:27:25 +0300

    Added simple array code.

---
+++ 07_Arrays_Simple Array Processing/Problem04_RotateArrayOfStrings/RotateArrayOfStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class RotateArrayOfStrings
+{
+    static void Main()
+    {
+        string values = Console.ReadLine();
+        string[] inputValues = values.Split(' ');
+
+        string elementsWithoutLastOne = "";
+        string lastElement = "";
+
+        for (int i = 0; i < inputValues.Length; i++)
+        {
+            if (i != inputValues.Length - 1)
+            {
+                elementsWithoutLastOne += inputValues[i] + " ";
+            }
+
+            else
+            {
+                lastElement += inputValues[i] + " ";
+            }
+        }
+
+        Console.WriteLine(lastElement + elementsWithoutLastOne);
+    }
+}
---
+++ 07_Arrays_Simple Array Processing/Problem06_OddNumbersAtOddPositions/OddNumbersAtOddPositions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class OddNumbersAtOddPositions
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        int element = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            element = int.Parse(numbers[i]);
+            if (i % 2 != 0 && element % 2 != 0)
+            {
+                Console.WriteLine($"Index {i} -> {element}");
+            }
+        }
+    }
+}
+
