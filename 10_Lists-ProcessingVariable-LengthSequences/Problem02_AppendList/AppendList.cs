458a36ae10c3a01fbcbdd6308469e91df4d80674
YoanEnchev <joanbojchev@abv.bg>
2017-04-07 12:12:54 +0300

    Added List exercises.

---
+++ 10_Lists-ProcessingVariable-LengthSequences/Problem02_AppendList/AppendList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class AppendList
+{
+    static void Main()
+    {
+        List<string> sequences = Console.ReadLine().Split('|').ToList();
+
+        string PartOfTheSequence = "";
+        string result = "";
+
+        for (int i = sequences.Count - 1; i >= 0; i--)
+        {
+            PartOfTheSequence = sequences[i];
+
+            PartOfTheSequence = EliminateNotNeededSpaces(PartOfTheSequence);
+
+            result += PartOfTheSequence;
+        }
+        Console.WriteLine(result);
+    }
+
+    public static string EliminateNotNeededSpaces(string partOfTheSequence)
+    {
+        string Reformated = "";
+        for (int i = 0; i < partOfTheSequence.Length; i++)
+        {
+            char symbol = partOfTheSequence[i];
+            if (symbol != ' ')
+            {
+                Reformated += partOfTheSequence[i] + " ";
+            }
+        }
+        return Reformated;
+    }
+}
+
